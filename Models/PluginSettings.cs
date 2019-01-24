6875dce1665a82275156f66864b959d279afaf82
Mattias Norell <mattias.norell@ninetech.com>
2019-01-24 15:14:22 +0100

    Removed unused file

--- Models/PluginSettings.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-namespace Couchpotato.Models{
-    public class PluginSettings{
-        public Dictionary<string, Dictionary<string, string>> Plugins{get;set;}
-    }
-}
